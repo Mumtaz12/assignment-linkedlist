637893986443c9213cf6d5df1c2beb3d13d6c21f
Mumtaz12 <41723154+Mumtaz12@users.noreply.github.com>
2018-08-21 11:49:40 +0530

    Add files via upload

--- countnodes.c
+++ countnodes.c
@@ -1,147 +1,114 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
 struct node {
     int data;
-    struct node * next;
-};
+    struct node *next;
+}*head;
 
-void createList(struct node **head, int n);
-void displayList(struct node *head);
-int  count(struct node *head);
+
+void createList(int n);
+int countNodes();
+void displayList();
 
 
 int main()
 {
-    int n, choice;
+    int n, total;
+
+    printf("Enter the total number of nodes: ");
+    scanf("%d", &n);
+    createList(n);
     
-    struct node *head = NULL;
+    printf("\nData in the list \n");
+    displayList();
     
-   
-    while(choice != 0)
-    {
-        printf("--------------------------------------------\n");
-        printf("        CIRCULAR LINKED LIST PROGRAM        \n");
-        printf("--------------------------------------------\n");
-        printf("1. Create List\n");
-        printf("2. Display list\n");
-        printf("3. Count nodes\n");
-        printf("0. Exit\n");
-        printf("--------------------------------------------\n");
-        
-        printf("Enter your choice : ");
-        scanf("%d", &choice);
-        
-        switch(choice)
-        {
-            case 1:
-                printf("Enter total node to create: ");
-                scanf("%d", &n);
-                createList(&head, n);
-                break;
-                
-            case 2:
-                displayList(head);
-                getchar();
-                getchar();
-                break;
-                
-            case 3:
-                printf("Total nodes = %d\n", count(head));
-                getchar();
-                getchar();
-                break;
-                
-            case 0:
-                printf("Exiting from application");
-                exit(0);
-                break;
-                
-            default:
-                printf("Error! Invalid choice. Please choose between 0-3");
-        }
-        
-        printf("\n\n\n\n\n");
-    }
+    total = countNodes();
+    
+    printf("\nTotal number of nodes = %d\n", total);
     
     return 0;
 }
 
-int count(struct node *head)
+void createList(int n)
 {
-    int total = 0;
-    struct node *current = head;
+    struct node *newNode, *temp;
+    int data, i;
     
-    
-    do
-    {
-        current = current->next;
-        total++;
-    } while (current != head);
-    
-    
-    return total;
-}
-
-void createList(struct node **head, int n)
-{
-    int i, data;
-    struct node *prevNode, *newNode;
-    
-    prevNode = NULL;
-    newNode  = NULL;
-    
-    
-   
-    for(i=1; i<=n; i++)
-    {
-        
-        newNode = (struct node *) malloc(sizeof(struct node));
-        
-        printf("Enter data of %d node: ", i);
-        scanf("%d", &data);
-        
-        newNode->data = data;
-        newNode->next = NULL;
-        
-        
-        if (prevNode != NULL)
-            prevNode->next = newNode;
-        
-        
-        prevNode = newNode;
-        
-        
-        if (*head == NULL)
-            *head = newNode;
-    }
-    
-    
-    prevNode->next = *head;
-    
-    printf("\nCIRCULAR LINKED LIST CREATED SUCCESSFULLY\n");
-}
-
-void displayList(struct node *head)
-{
-    struct node *current;
-    int n = 1;
-    
+    head = (struct node *)malloc(sizeof(struct node));
     
     if(head == NULL)
     {
-        printf("List is empty.\n");
-        return;
+        printf("Unable to allocate memory.");
+    }
+    else
+    {
+        
+        printf("Enter the data of node 1: ");
+        scanf("%d", &data);
+        
+        head->data = data;
+        head->next = NULL;
+        temp = head;
+        
+        for(i=2; i<=n; i++)
+        {
+            newNode = (struct node *)malloc(sizeof(struct node));
+            
+                      if(newNode == NULL)
+            {
+                printf("Unable to allocate memory.");
+                break;
+            }
+            else
+            {
+                printf("Enter the data of node %d: ", i);
+                scanf("%d", &data);
+                
+                newNode->data = data;
+                newNode->next = NULL;
+                temp->next = newNode;
+                
+                temp = temp->next;
+            }
+        }
+        
+        printf("SINGLY LINKED LIST CREATED SUCCESSFULLY\n");
+    }
+}
+int countNodes()
+{
+    int count = 0;
+    struct node *temp;
+    
+    temp = head;
+    
+    while(temp != NULL)
+    {
+        count++;
+        temp = temp->next;
     }
     
+    return count;
+}
+
+void displayList()
+{
+    struct node *temp;
     
-    current = head;
-    printf("DATA IN THE LIST:\n");
-    
-    do
+    if(head == NULL)
     {
-        printf("Data %d = %d\n", n++, current->data);
-        
-      current = current->next;
-    } while (current != head);
+        printf("List is empty.");
+    }
+    else
+    {
+        temp = head;
+        while(temp != NULL)
+        {
+            printf("Data = %d\n", temp->data);
+            temp = temp->next;
+        }
+    }
 }
